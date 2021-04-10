8f5f96594eea1d8ea140f90599f042025618a185
Shuai Shao <shaoshuai@acm.org>
2021-04-10 15:03:42 +0800

    update GUI

--- client.py
+++ client.py
@@ -2,16 +2,21 @@ import grpc
 from grpc_lib import texas_pb2_grpc, texas_pb2
 import time
 import json
+from render import Render
+
 user_info = None
+
+
 def regist(input_user_info):
-    channel = grpc.insecure_channel('localhost:50051')
+    channel = grpc.insecure_channel("localhost:50051")
     stub = texas_pb2_grpc.TexasStub(channel)
     response = stub.UserRegister(input_user_info)
     print(response)
     return response
 
+
 def login(input_user_info):
-    channel = grpc.insecure_channel('localhost:50051')
+    channel = grpc.insecure_channel("localhost:50051")
     stub = texas_pb2_grpc.TexasStub(channel)
     response = stub.UserLogin(input_user_info)
     print(response)
@@ -19,89 +24,123 @@ def login(input_user_info):
 
 
 def create_room():
-    channel = grpc.insecure_channel('localhost:50051')
+    channel = grpc.insecure_channel("localhost:50051")
     stub = texas_pb2_grpc.TexasStub(channel)
-    req = texas_pb2.CreateRoomRequest(room_name='my_test_room', blind=5, buyin=200, user_info=user_info)
+    req = texas_pb2.CreateRoomRequest(
+        room_name="my_test_room", blind=5, buyin=200, user_info=user_info
+    )
     response = stub.CreateRoom(req)
     print(response)
     return response
 
+
 def getin_room(room_id):
-    channel = grpc.insecure_channel('localhost:50051')
+    channel = grpc.insecure_channel("localhost:50051")
     stub = texas_pb2_grpc.TexasStub(channel)
     req = texas_pb2.GetinRoomRequest(room_id=room_id, user_info=user_info)
     response = stub.GetinRoom(req)
     print(response)
     return response
 
+
 def get_status(room_id):
-    channel = grpc.insecure_channel('localhost:50051')
+    channel = grpc.insecure_channel("localhost:50051")
     stub = texas_pb2_grpc.TexasStub(channel)
-    response = stub.GetStatus(texas_pb2.GetStatusRequest(user_info=user_info, room_id=room_id))
+    response = stub.GetStatus(
+        texas_pb2.GetStatusRequest(user_info=user_info, room_id=room_id)
+    )
     return response
+
+
 def send_action(extra, room_id):
-    channel = grpc.insecure_channel('localhost:50051')
+    channel = grpc.insecure_channel("localhost:50051")
     stub = texas_pb2_grpc.TexasStub(channel)
-    response = stub.Action(texas_pb2.ActionRequest(user_info=user_info, room_id=room_id, extra=json.dumps(extra)))
+    response = stub.Action(
+        texas_pb2.ActionRequest(
+            user_info=user_info, room_id=room_id, extra=json.dumps(extra)
+        )
+    )
     print(response)
     return response
-   
+
+
 def get_myid(status):
     for player in status.room_status.players:
-        print('player name = {}, user name = {}, equal = {}'.format(player.player_name, user_info.user_name, player.player_name == user_info.user_name))
+        print(
+            "player name = {}, user name = {}, equal = {}".format(
+                player.player_name,
+                user_info.user_name,
+                player.player_name == user_info.user_name,
+            )
+        )
         if player.player_name == user_info.user_name:
             return player.player_id
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     begin_get = False
     room_id = None
-    while True: 
-        st = input("input your action\nr : regist\nl : login\nc : create room\ng : get in room\n")
-        if st == 'r':
+    render = Render()
+
+    while True:
+        st = input(
+            "input your action\nr : regist\nl : login\nc : create room\ng : get in room\n"
+        )
+        if st == "r":
             user_name = input("input your user name\n").strip()
             pass_wd = input("input your password\n").strip()
             regist_user_info = texas_pb2.UserInfo(user_name=user_name, passwd=pass_wd)
             rsp = regist(regist_user_info)
             if rsp.code == 0:
                 user_info = regist_user_info
-        if st == 'l':
+        if st == "l":
             user_name = input("input your user name\n").strip()
             pass_wd = input("input your password\n").strip()
             login_user_info = texas_pb2.UserInfo(user_name=user_name, passwd=pass_wd)
             rsp = login(login_user_info)
             if rsp.code == 0:
                 user_info = login_user_info
-        if st == 'c':
+        if st == "c":
             create_room()
-        if st == 'g':
+        if st == "g":
             room_id = int(input("input room id\n").strip())
             rsp = getin_room(room_id)
-            if (rsp.code == 0):
+            if rsp.code == 0:
                 break
+
     pre_status = None
+    pre_time = time.time()
     while True:
         status = get_status(room_id)
         time.sleep(0.5)
         if status == pre_status:
             continue
         else:
-            print(status)
-            pre_status = status
+            print(type(status), type(status.room_status))
             myid = get_myid(status)
-            print('myid = {}'.format(myid))
-            if status.room_status.speak == 0 and status.room_status.players[myid - 1].ready:
+            render.parse(status.room_status, myid)
+            render.draw()
+
+            pre_status = status
+            pre_time = time.time()
+
+            if (
+                status.room_status.speak == 0
+                and status.room_status.players[myid - 1].ready
+            ):
                 continue
             if status.room_status.speak == 0:
-                st = input("input your action\nready\nquit\n")
-                if st == 'ready':
-                    send_action({'action': 'ready'}, room_id) 
-                if st == 'quit':
-                    send_action({'action': 'quit'}, room_id)
+                st = input("input your action:[ready][quit]\n")
+                if st == "ready":
+                    send_action({"action": "ready"}, room_id)
+                if st == "quit":
+                    send_action({"action": "quit"}, room_id)
             if myid == status.room_status.speak:
-                st = input("input your action\ncall\ncheck\nfold\nraise\n")
-                if st == 'call' or st == 'check':
-                    send_action({'action': 'call'}, room_id)
-                if st == 'fold': 
-                    send_action({'action': 'fold'}, room_id)
-                if st == 'raise':
+                st = input("input your action:[call][check][fold][raise]\n")
+                if st == "call" or st == "check":
+                    send_action({"action": "call"}, room_id)
+                if st == "fold":
+                    send_action({"action": "fold"}, room_id)
+                if st == "raise":
                     target = int(input("input your target"))
-                    send_action({'action': 'call', 'raise_target': target}, room_id)
+                    send_action({"action": "call", "raise_target": target}, room_id)
--- render.py
+++ render.py
@@ -1,59 +1,104 @@
-# from enum import Enum
-# value = [" A", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9", "10", " J", " Q", " K"]
-# suit = ["♠", "♣", "♥", "♦"]
-# Value = Enum("Value", ("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"))
-# Suit = Enum("Suit", ("♠", "♣", "♥", "♦"))
+from grpc_lib import texas_pb2
+
+PRETTY_VALUE = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+PRETTY_SUITS = {
+    1: "♠",  # spades
+    2: "♥",  # hearts
+    4: "♦",  # diamonds
+    8: "♣",  # clubs
+}
+
 
 def fixed_length(s, length=10):
     if len(s) <= length:
         return s + " " * (length - len(s))
-    return s[:length - 3] + "..."
+    return s[: length - 3] + "..."
+
 
 class Card:
-    def __init__(self, value="", suit=""):
-        self.value = value
-        self.suit = suit
+    def __init__(self, card: texas_pb2.Poker = None):
+        self.value = "  "
+        self.suit = "  "
+        if card is not None:
+            self.parse(card)
+
+    def parse(self, card: texas_pb2.Poker):
+        self.value = PRETTY_VALUE[card.suit]
+        self.suit = PRETTY_SUITS[card.value]
         self.colored()
 
     def colored(self):
         if self.suit in ["♥", "♦"]:
             color = "\033[31m"  # red
         else:
-            color = "\033[0m"   # default
+            color = "\033[0m"  # default
         self.value = "{}{}\033[0m".format(color, fixed_length(self.value, 2))
         self.suit = "{}{}\033[0m".format(color, fixed_length(self.suit, 2))
 
+
 class Player:
-    def __init__(self, name="", chips=0):
-        self.name = name
-        self.chips = chips
-        self.bet = 0
-        self.stat = ""
-        self.hand = []
+    def __init__(self, player_status: texas_pb2.PlayerStatus = None):
+        self.player_name = ""
+        self.player_id = -1
+        self.counter = 0
+        self.buyin_time = 0
+        self.active = False
+        self.ready = False
+        self.pool = 0
+        self.hands = []
+        if player_status is not None:
+            self.parse(player_status)
 
-    def set_name(self, name):
-        self.name = name
+    def parse(self, player_status: texas_pb2.PlayerStatus):
+        self.player_name = player_status.player_name
+        self.player_id = player_status.player_id
+        self.counter = player_status.counter
+        self.buyin_time = player_status.buyin_time
+        self.active = player_status.active
+        self.ready = player_status.ready
+        self.pool = player_status.pool
+        if player_status.active:
+            self.hands = [Card(), Card()]
+        if len(player_status.hands) == 2:
+            self.hands[0].parse(player_status.hands[0])
+            self.hands[1].parse(player_status.hands[1])
 
-    def set_chips(self, chips):
-        self.chips = chips
-
-    def set_hand(self, card1, card2):
-        self.hand = [card1, card2]
 
 class Render:
     def __init__(self):
         self.player = [Player() for i in range(9)]
         self.public = [Card() for i in range(5)]
-    
+        self.myid = 0
+
+    def parse(self, status: texas_pb2.RoomStatus, myid):
+        self.__init__()
+        self.myid = myid
+        for player in status.players:
+            self.player[player.player_id - self.myid].parse(player)
+        for i in range(len(status.public)):
+            self.public[i].parse(status.public[i])
+
     def draw(self):
         def card_box(pos, i):
-            if i >= 0 and len(self.player[i].hand) == 2:
+            if i >= 0 and len(self.player[i].hands) == 2:
                 if pos == 0:
                     print(" ┌──╥──┐       ", end="")
                 elif pos == 1:
-                    print(" │{}║{}│       ".format(self.player[i].hand[0].value, self.player[i].hand[1].value), end="")
+                    print(
+                        " │{}║{}│       ".format(
+                            self.player[i].hands[0].value, self.player[i].hands[1].value
+                        ),
+                        end="",
+                    )
                 elif pos == 2:
-                    print(" │{}║{}│${:<5} ".format(self.player[i].hand[0].suit, self.player[i].hand[1].suit, self.player[i].bet), end="")
+                    print(
+                        " │{}║{}│${:<5} ".format(
+                            self.player[i].hands[0].suit,
+                            self.player[i].hands[1].suit,
+                            self.player[i].pool,
+                        ),
+                        end="",
+                    )
                 elif pos == 3:
                     print(" └──╨──┘       ", end="")
             else:
@@ -65,9 +110,20 @@ class Render:
                 if pos == 0:
                     print("┌─────────────┐", end="")
                 elif pos == 1:
-                    print("│{:<10}   │".format(fixed_length(self.player[i].name, 10)), end="")
+                    print(
+                        "│{:<10}   │".format(
+                            fixed_length(self.player[i].player_name, 10)
+                        ),
+                        end="",
+                    )
                 elif pos == 2:
-                    print("│{:<4}  ${:>6}│".format(self.player[i].stat, self.player[i].chips, ), end="")
+                    print(
+                        "│{:<4}  ${:>6}│".format(
+                            "",
+                            self.player[i].counter,
+                        ),
+                        end="",
+                    )
                 elif pos == 3:
                     print("└─────────────┘", end="")
             else:
@@ -81,12 +137,36 @@ class Render:
             for i in [1, 2, 3, 4, 5]:
                 card_box(pos, i)
             print()
-        print("╭─────────────────────────────────────────────────────────────────────────╮")
-        print("│                            ┌──╥──╥──╥──╥──┐                             │")
-        print("│                            │{}║{}║{}║{}║{}│                             │".format(self.public[0].value, self.public[1].value, self.public[2].value, self.public[3].value, self.public[4].value))
-        print("│                            │{}║{}║{}║{}║{}│                             │".format(self.public[0].suit, self.public[1].suit, self.public[2].suit, self.public[3].suit, self.public[4].suit))
-        print("│                            └──╨──╨──╨──╨──┘                             │")
-        print("╰─────────────────────────────────────────────────────────────────────────╯")
+        print(
+            "╭─────────────────────────────────────────────────────────────────────────╮"
+        )
+        print(
+            "│                            ┌──╥──╥──╥──╥──┐                             │"
+        )
+        print(
+            "│                            │{}║{}║{}║{}║{}│                             │".format(
+                self.public[0].value,
+                self.public[1].value,
+                self.public[2].value,
+                self.public[3].value,
+                self.public[4].value,
+            )
+        )
+        print(
+            "│                            │{}║{}║{}║{}║{}│                             │".format(
+                self.public[0].suit,
+                self.public[1].suit,
+                self.public[2].suit,
+                self.public[3].suit,
+                self.public[4].suit,
+            )
+        )
+        print(
+            "│                            └──╨──╨──╨──╨──┘                             │"
+        )
+        print(
+            "╰─────────────────────────────────────────────────────────────────────────╯"
+        )
         for pos in [0, 1, 2, 3]:
             for i in [0, -1, 8, 7, 6]:
                 card_box(pos, i)
@@ -95,31 +175,3 @@ class Render:
             for i in [0, -1, 8, 7, 6]:
                 user_box(pos, i)
             print()
-
-if __name__ == "__main__":
-    render = Render()
-    render.player[0].set_name("ShaoShuai")
-    render.player[1].set_name("hyuxusnia")
-    render.player[2].set_name("nhaxyuuis")
-    render.player[3].set_name("xsuhyinau")
-    render.player[6].set_name("xuiyausnh")
-    render.player[7].set_name("uxyunihas")
-    render.player[8].set_name("auxsuhnyi")
-    render.player[0].set_chips(1)
-    render.player[1].set_chips(12)
-    render.player[2].set_chips(123)
-    render.player[3].set_chips(1234)
-    render.player[6].set_chips(100)
-    render.player[7].set_chips(1000)
-    render.player[8].set_chips(10000)
-    render.player[1].stat = "[BB]"
-    render.player[2].stat = "[SB]"
-    render.player[3].stat = "[D]"
-    render.player[1].hand = [Card("A", "♠"), Card("10", "♦")]
-    render.player[2].hand = [Card("", ""), Card("", "")]
-    render.player[3].hand = [Card("", ""), Card("", "")]
-    render.player[0].hand = [Card("K", "♠"), Card("K", "♦")]
-    render.player[7].hand = [Card("", ""), Card("", "")]
-    render.player[3].bet = 125
-    render.public = [Card("A", "♠"), Card("9", "♥"), Card("10", "♦"), Card(), Card()]
-    render.draw()
