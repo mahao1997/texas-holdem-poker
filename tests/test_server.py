215d8a3a2b37720ef9151b3317e1744c7ba66622
Shuai Shao <shaoshuai@acm.org>
2021-04-10 17:55:26 +0800

    add test case for regist and login

--- tests/test_server.py
+++ tests/test_server.py
@@ -4,52 +4,53 @@ import grpc
 from grpc_lib import texas_pb2_grpc, texas_pb2
 from concurrent import futures
 from texas_server import Texas
-from client import regist, login
-import time
+
 
 class TestServer(unittest.TestCase):
     def setUp(self):
         self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
         texas_pb2_grpc.add_TexasServicer_to_server(Texas(), self.server)
-        self.server.add_insecure_port('[::]:50051')
+        self.server.add_insecure_port("[::]:50051")
         self.server.start()
 
     def test_regist_and_login(self):
+        channel = grpc.insecure_channel("localhost:50051")
+        stub = texas_pb2_grpc.TexasStub(channel)
+
         # regist
-        regist_user_info = texas_pb2.UserInfo(user_name="username", passwd="password")
-        rsp = regist(regist_user_info)
+        user_info = texas_pb2.UserInfo(user_name="username", passwd="password")
+        rsp = stub.UserRegister(user_info)
         self.assertEqual(rsp.code, 0)
 
         # regist same name
-        regist_user_info = texas_pb2.UserInfo(user_name="username", passwd="password")
-        rsp = regist(regist_user_info)
+        user_info = texas_pb2.UserInfo(user_name="username", passwd="password")
+        rsp = stub.UserRegister(user_info)
         self.assertEqual(rsp.code, -1)
 
         # regist empty name
-        regist_user_info = texas_pb2.UserInfo(user_name="", passwd="password")
-        rsp = regist(regist_user_info)
+        user_info = texas_pb2.UserInfo(user_name="", passwd="password")
+        rsp = stub.UserRegister(user_info)
         self.assertEqual(rsp.code, -1)
 
         # regist empty password
-        regist_user_info = texas_pb2.UserInfo(user_name="username", passwd="")
-        rsp = regist(regist_user_info)
+        user_info = texas_pb2.UserInfo(user_name="username", passwd="")
+        rsp = stub.UserRegister(user_info)
         self.assertEqual(rsp.code, -1)
 
         # login
-        login_user_info = texas_pb2.UserInfo(user_name="username", passwd="password")
-        rsp = login(login_user_info)
+        user_info = texas_pb2.UserInfo(user_name="username", passwd="password")
+        rsp = stub.UserLogin(user_info)
         self.assertEqual(rsp.code, 0)
 
         # wrong username
-        login_user_info = texas_pb2.UserInfo(user_name="no one", passwd="password")
-        rsp = login(login_user_info)
+        user_info = texas_pb2.UserInfo(user_name="no one", passwd="password")
+        rsp = stub.UserLogin(user_info)
         self.assertEqual(rsp.code, -1)
 
         # wrong password
-        login_user_info = texas_pb2.UserInfo(user_name="username", passwd="")
-        rsp = login(login_user_info)
+        user_info = texas_pb2.UserInfo(user_name="username", passwd="")
+        rsp = stub.UserLogin(user_info)
         self.assertEqual(rsp.code, -1)
-
 
     def tearDown(self):
         self.server.stop(0)
